708658ae642ba621d02fff66b15ab0c81f1da7b7
ntaro <taro.nagasawa@gmail.com>
2014-10-24 14:47:05 +0900

    OkHttp使用

--- app/src/main/kotlin/com/cococlip/android/client/ApiClient.kt
+++ app/src/main/kotlin/com/cococlip/android/client/ApiClient.kt
@@ -1,19 +1,29 @@
 package com.cococlip.android.client
 
 import com.cococlip.android.model.Clip
-import java.net.URL
 import org.json.JSONObject
 import org.json.JSONArray
 import org.funktionale.either.Either
 import org.funktionale.either.either
 import com.cococlip.android.model.Location
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Response
 
 /**
  * @author Taro Nagasawa
  */
 public object ApiClient {
 
-    private fun String.readText(): String = URL("https://cococlip.herokuapp.com" + this).readText("UTF-8")
+    private val client = OkHttpClient()
+
+    private fun Request.Builder.path(path: String): Request.Builder = url("https://cococlip.herokuapp.com" + path)
+
+    private fun Request.Builder.executeBy(client: OkHttpClient): Response = client.newCall(build()).execute()
+
+    private fun String.toJsonObject(): JSONObject = JSONObject(this)
+
+    private fun JSONObject.findString(key: String): String? = if (has(key)) getString(key) else null
 
     private fun <T> JSONArray.map(f: (JSONObject) -> T): List<T> {
         val builder = ImmutableArrayListBuilder<T>()
@@ -26,24 +36,27 @@ public object ApiClient {
 
     public fun getClips(latitude: Double, longitude: Double): Either<Exception, List<Clip>> {
         return either {
-            val response = "/api/1/clips?lat=${latitude}&lon=${longitude}".readText()
+            Request.Builder()
+                    .path("/api/1/clips?lat=${latitude}&lon=${longitude}")
+                    .get()
+                    .executeBy(client)
+                    .body()
+                    .string()
+                    .toJsonObject()
+                    .getJSONArray("results")
+                    .map {
+                        val loc = it.getJSONObject("loc")
 
-            val jsonObject = JSONObject(response)
-            jsonObject.getJSONArray("results").map {
-                val loc = it.getJSONObject("loc")
-
-                Clip(
-                        id = it.getString("_id"),
-                        title = it.getString("title"),
-                        location = Location(loc.getDouble("lat"), loc.getDouble("lon")),
-                        image1Url = it.findString("high_image1_url"),
-                        image2Url = it.findString("high_image2_url"),
-                        thumbnail1Url = it.findString("low_image1_url"),
-                        thumbnail2Utl = it.findString("low_image2_url")
-                )
-            }
+                        Clip(
+                                id = it.getString("_id"),
+                                title = it.getString("title"),
+                                location = Location(loc.getDouble("lat"), loc.getDouble("lon")),
+                                image1Url = it.findString("high_image1_url"),
+                                image2Url = it.findString("high_image2_url"),
+                                thumbnail1Url = it.findString("low_image1_url"),
+                                thumbnail2Utl = it.findString("low_image2_url")
+                        )
+                    }
         }
     }
-
-    private fun JSONObject.findString(key: String): String? = if (has(key)) getString(key) else null
 }
