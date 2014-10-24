ab20158910079f9fcc328780970a7df492e6804d
ntaro <taro.nagasawa@gmail.com>
2014-10-24 14:17:57 +0900

    サムネイル取得

--- app/src/main/kotlin/com/cococlip/android/app/ClipListItemView.kt
+++ app/src/main/kotlin/com/cococlip/android/app/ClipListItemView.kt
@@ -7,11 +7,15 @@ import com.cococlip.android.R
 import android.widget.TextView
 import com.cococlip.android.model.Clip
 import butterknife.bindView
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 /**
  * @author Taro Nagasawa
  */
 public class ClipListItemView(context: Context) : FrameLayout(context) {
+
+    private val thumbnailView: ImageView by bindView(R.id.thumbnail_view)
 
     private val titleView: TextView by bindView(R.id.title_view)
 
@@ -30,6 +34,10 @@ public class ClipListItemView(context: Context) : FrameLayout(context) {
     public fun bind(clip: Clip) {
         titleView.setText(clip.title)
         addressView.setText(clip.location.getTextForDisplay())
+
+        clip.thumbnail1Url?.let {
+            Picasso.with(getContext()).load(it).into(thumbnailView)
+        }
     }
 
     public class object {
